e0c96380c53d7dfbf6146f3f474710edf4469923
Mohamed MOUNIR <mohamed.mounir.smb@gmail.com>
2023-06-03 15:17:27 +0100

    init 2

--- app/Http/Controllers/Homecontroller.php
+++ app/Http/Controllers/Homecontroller.php
@@ -3,12 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\contact;
-use App\Models\delivery;
-use App\Models\health;
-use App\Models\cleaning;
-use App\Models\babysitting;
-use App\Models\gardener;
-use App\Models\homerepair;
+use App\Models\booking;
 use App\Models\data;
 use Illuminate\Http\Request;
 
@@ -78,25 +73,6 @@ public function contactusafter(Request $request)
     return redirect()->route('app_contactusbefore')->with('success', 'Message sent!');
 }
 public function store(Request $request){{
-    // $input = $request->all();
-    // $b_exists = data::where('first_name','=',$input['first_name'])->exists();
-    // if($b_exists){
-    //     session()->flash('Error','client aready exists');
-    //     return redirect('/data');
-    // }
-    // else{
-        // data::create([
-        //     'first_name' => $request->first_name,
-        //     'second_name' => $request->second_name,
-        //     'email' => $request->email,
-        //     'adress' => $request->adress,
-        //     'city' => $request->city,
-        //     'code_postal' => $request->code_postal,
-        //     'service' => $request->service,
-        //     'plan' => $request->plan,
-        // ]);
-        // session()->flash('Add','client added successfuly');
-        // return redirect('/data');
         $validateData = $request->validate([
 
             'first_name' => 'required',
@@ -104,9 +80,8 @@ public function store(Request $request){{
             'email' => 'required',
             'adress' => 'required',
             'city' => 'required',
-            'code_postal' => 'required',
-            'service' => 'required',
-            'plan' => 'required',
+            'zip' => 'required',
+            'services' => 'required',
         ]);
 
         $table = new data;
@@ -115,8 +90,9 @@ public function store(Request $request){{
         $table->email = $request->email;
         $table->adress = $request->adress;
         $table->city = $request->city;
-        $table->service = $request->service;
-        $table->plan = $request->plan;
+        $table->zip = $request->zip;
+        $table->services = $request->services;
+
         $table->save();
         return redirect()->route('data')->with('success', 'Details sent!');
     }
@@ -127,23 +103,6 @@ public function homecleaning()
 {
     return view('home.homecleaning');
 }
-public function homecleaningafter(Request $request)
-{
-    $validateData = $request->validate([
-        'name' => 'required',
-        'number' => 'required',
-        'adress' => 'required',
-        'service' => 'required',
-    ]);
-
-    $table = new cleaning;
-    $table->name = $request->name;
-    $table->number = $request->number;
-    $table->adress = $request->adress;
-    $table->service = $request->service;
-    $table->save();
-    return redirect()->route('app_homecleaning')->with('success', 'Details sent!');
-}
 
 
 //La page/vue babysitting.blade.php
@@ -151,68 +110,18 @@ public function babysitting()
 {
     return view('home.babysitting');
 }
-public function babysittingafter(Request $request)
-{
-    $validateData = $request->validate([
-        'name' => 'required',
-        'number' => 'required',
-        'adress' => 'required',
-        'service' => 'required',
-    ]);
-
-    $table = new babysitting;
-    $table->name = $request->name;
-    $table->number = $request->number;
-    $table->adress = $request->adress;
-    $table->service = $request->service;
-    $table->save();
-    return redirect()->route('app_babysitting')->with('success', 'Details sent!');
-}
 
 //La page/vue homerepair.blade.php
 public function homerepair()
 {
     return view('home.homerepair');
 }
-public function homerepairafter(Request $request)
-{
-    $validateData = $request->validate([
-        'name' => 'required',
-        'number' => 'required',
-        'adress' => 'required',
-        'service' => 'required',
-    ]);
 
-    $table = new homerepair;
-    $table->name = $request->name;
-    $table->number = $request->number;
-    $table->adress = $request->adress;
-    $table->service = $request->service;
-    $table->save();
-    return redirect()->route('app_homerepair')->with('success', 'Details sent!');
-}
 
 //La page/vue gardener.blade.php
 public function gardener()
 {
     return view('home.gardener');
-}
-public function gardenerafter(Request $request)
-{
-    $validateData = $request->validate([
-        'name' => 'required',
-        'number' => 'required',
-        'adress' => 'required',
-        'service' => 'required',
-    ]);
-
-    $table = new gardener;
-    $table->name = $request->name;
-    $table->number = $request->number;
-    $table->adress = $request->adress;
-    $table->service = $request->service;
-    $table->save();
-    return redirect()->route('app_gardener')->with('success', 'Details sent!');
 }
 
 //La page/vue delivery.blade.php
@@ -220,31 +129,19 @@ public function delivery()
 {
     return view('home.delivery');
 }
-public function deliveryafter(Request $request)
-{
-    $validateData = $request->validate([
-        'name' => 'required',
-        'number' => 'required',
-        'adress' => 'required',
-        'service' => 'required',
-    ]);
 
-    $table = new delivery;
-    $table->name = $request->name;
-    $table->number = $request->number;
-    $table->adress = $request->adress;
-    $table->service = $request->service;
-    $table->save();
-    return redirect()->route('app_delivery')->with('success', 'Details sent!');
-}
 
 //La page/vue health.blade.php
 public function health()
 {
     return view('home.health');
 }
-public function healthafter(Request $request)
+
+public function adminhome()
 {
+    return view('home.dashboard');
+}
+public function booking(Request $request){{
     $validateData = $request->validate([
         'name' => 'required',
         'number' => 'required',
@@ -252,13 +149,13 @@ public function healthafter(Request $request)
         'service' => 'required',
     ]);
 
-    $table = new health;
+    $table = new booking;
     $table->name = $request->name;
     $table->number = $request->number;
     $table->adress = $request->adress;
     $table->service = $request->service;
     $table->save();
-    return redirect()->route('app_health')->with('success', 'Details sent!');
+    return redirect()->route('app_home');
 }
 public function adminhome()
 {
